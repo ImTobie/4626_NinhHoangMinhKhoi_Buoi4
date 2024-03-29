b343ade59a668e4db7628de1ba909794776e5be5
Minh Khoi <ninhkhoi36@gmail.com>
2024-03-29 15:08:12 +0700

    Add project files.

---
+++ Buoi3/Controllers/CategoryController.cs
@@ -0,0 +1,121 @@
+﻿using Buoi3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buoi3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+        // Hiển thị danh sách category
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return View(categories);
+        }
+        // Hiển thị form thêm sản phẩm mới
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoryRepository.AddAsync(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,CreatedAt,UpdatedAt")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryRepository.UpdateAsync(category);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await CategoryExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                await _categoryRepository.DeleteAsync(id);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> CategoryExists(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            return category != null;
+        }
+    }
+
+}
